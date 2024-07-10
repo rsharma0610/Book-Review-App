5872a6ce4939066a1fc1b69aea99026c767991c2
Rohan Sharma <rohansharma@Rohans-MacBook-Pro.local>
2024-07-09 23:05:19 -0400

    Index.ejs file clean up success

--- index.js
+++ index.js
@@ -17,28 +17,24 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let currentUserId = 0;
+
 async function validUser(username, password){
-    const query = await db.query("SELECT username, password FROM users WHERE username=$1 AND password=$2", [username, password]);
-    //console.log(query.rows);
+    const query = await db.query("SELECT id, username, password FROM users WHERE username=$1 AND password=$2", [username, password]);
     if(query.rows.length !== 0){
-        //console.log("Proceed into website");
+        currentUserId = query.rows[0].id;
         return true;
     }
-    //console.log("User does not exist");
     return false;
 }
-async function getUserId(username, password){
-    const query = await db.query("SELECT id FROM users WHERE username=$1 AND password=$2", [username, password]);
-    return query.rows[0].id;
-}
-async function getReviews(username, password){
+
+async function getReviews(id){
     const query = await db.query(
-        "SELECT books.title, books.review, books.rating, books.date, users.name FROM books JOIN users ON books.userId = users.id WHERE users.username = $1 AND users.password = $2;",
-        [username, password]);
+        "SELECT books.title, books.review, books.rating, books.date, users.name FROM books JOIN users ON books.userId = users.id WHERE users.id = $1;",
+        [id]);
     return query.rows;
 }
 
-let currentUserId = 0;
 console.log("User id before the login", currentUserId);
 //Gets the login view
 app.get("/", async (req, res) => {
@@ -56,9 +52,8 @@ app.get("/create", (req, res) => {
 app.post("/login", async (req, res) => {
     const userExists = await validUser(req.body.username, req.body.password);
     if(userExists){
-        currentUserId = await getUserId(req.body.username, req.body.password);
         console.log("User id after the login", currentUserId);
-        var reviews = await getReviews(req.body.username, req.body.password);
+        var reviews = await getReviews(currentUserId);
         res.render("index.ejs", {reviews: reviews});
     }else{
         res.render("login.ejs", {error: "Invalid Credentials, Try Again or Register an Account"});
@@ -83,7 +78,7 @@ app.post("/publish", async(req, res) => {
         await db.query("INSERT INTO books (userId, title, author, review, rating) VALUES ($1, $2, $3, $4, $5);",
         [currentUserId, req.body.title, req.body.author, req.body.review, req.body.rating]);
     }catch(error){}
-    
+    var reviews = await getReviews(currentUserId);
     res.render("index.ejs", {reviews: reviews});
 })
 //Sorts the reviews, make sure to update the sort type displayed in the index.ejs file
